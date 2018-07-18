158540c1e73c470c9274ec228ea7603ed27fa03e
dantegates <dantegates@hotmail.com>
2018-07-18 16:12:54 -0400

    using transformer API

--- feature_columns.py
+++ feature_columns.py
@@ -1,4 +1,5 @@
 import keras
+import numpy as np
 
 
 class FeatureColumn:
@@ -19,35 +20,56 @@ class NumericColumn(FeatureColumn):
         super().__init__(*args, **kwargs)
         self.normalizer = normalizer
 
+    def fit(self, X):
+        if self.normalizer is not None:
+            self.normalizer.fit(X)
+        return self
+
     def transform(self, X):
         if self.normalizer is not None:
-            return self.normalizer(X).values
+            return self.normalizer.transform(X).values
         return X.values
-
-
+    
+    
 class EmbeddingColumn(FeatureColumn):
-    def __init__(self, *args, vocabulary, output_dim, **kwargs):
+    def __init__(self, *args, vocab_size, output_dim, **kwargs):
         super().__init__(*args, **kwargs)
-        self.vocabulary = vocabulary
-        self.vocab_map = {v: i for i, v in enumerate(vocabulary)}
+        self.vocab_size = vocab_size
         self.output_dim = output_dim
+
+    def fit(self, X):
+        self.vocab_map = {v: i for i, v in enumerate(set(X))}
+        @np.vectorize
+        def apply_mapping(X):
+            return self.vocab_map.get(x, self.vocab_size)
+        self._apply_mapping = apply_mapping
+        return self
+
+    def transform(self, X):
+        return self._apply_mapping(X)
 
     @property
     def output(self):
         embedding = keras.layers.Embedding(
-            input_dim=len(self.vocabulary)+1,  # +1 for OOV
+            input_dim=self.vocab_size+1,  # +1 for OOV
             output_dim=self.output_dim,
             input_length=1)(self.input)
         return keras.layers.Flatten()(embedding)
 
-    def transform(self, X):
-        mapping = lambda x: self.vocab_map.get(x, len(self.vocabulary))
-        return X.apply(mapping).values
 
-
-class Features:
+class FeatureSet:
     def __init__(self, *features):
         self.features = features
+
+    def fit(self, X):
+        self.features = [f.fit(X[f.name]) for f in self.features]
+        return self
+
+    def transform(self, X):
+        return [f.transform(X[f.name]) for f in self.features]
+
+    def fit_transform(self, X):
+        return self.fit(X).transform(X)
 
     @property
     def inputs(self):
@@ -58,38 +80,15 @@ class Features:
         concat = keras.layers.Concatenate(axis=-1)
         return concat([f.output for f in self.features])
 
+
+class Scaler:
+    def __init__(self):
+        self.mean = self.std = None
+
+    def fit(self, X):
+        self.mean = np.mean(X)
+        self.std = np.std(X)
+        return self
+
     def transform(self, X):
-        return [f.transform(X[f.name]) for f in self.features]
-
-
-if __name__ == '__main__':
-    import pandas as pd
-    import numpy as np
-
-    def normalize(column):
-        mean = X[column].mean()
-        std = X[column].std()
-        def normalizer(X, mean=mean, std=std):
-            return (X - std) / mean
-        return normalizer
-
-    X = pd.DataFrame({
-        'feature1': np.random.randint(10, size=100),
-        'feature2': np.random.randint(100, size=100),
-        'feature3': np.random.rand(size=100)
-    })
-    y = np.random.rand(size=100)
-
-
-    features = Features(
-        EmbeddingColumn('feature1', output_dim=10),
-        EmbeddingColumn('feature2', output_dim=10),
-        NumericColumn('feature3'),
-    )
-
-    x = keras.layers.Dense(50, activation='relu')(features.output)
-    x = keras.layers.Dense(50, activation='relu')(x)
-    x = keras.layers.Dense(1, activation='sigmoid')
-    model = keras.models.Model(inputs=features.inputs, outputs=x)
-    model.compile(loss='mean_squarred_error', optimizer='adam')
-    model.fit(features.transform(X), y)
+        return (X - self.mean) / self.std
